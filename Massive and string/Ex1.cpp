56800ca7b1905ca7922cdd6c147e0670a42f83fe
rabbritva <rabbritva@gmail.com>
2026-04-21 12:06:50 +0500

    Update Ex1.cpp

--- Massive and string/Ex1.cpp
+++ Massive and string/Ex1.cpp
@@ -4,61 +4,59 @@
 using namespace std;
 
 const short N = 7;
-void sorted(int*);
-void show(int*, int);
-void concat(int mas[], int*, int*, int);
+const short M = 5;
 
-int main(){
+void sorted(int*, short);
+void show(int*, short);
+int* concat(int*, short, int*, short, short&);
+
+int main() {
 	system("chcp 1251 > nul");
 	srand(time(0));
-	int m1[N], m2[N];
-	for(unsigned i = 0; i<N; i++)  {m1[i] = rand()%101; m2[i] = rand()%11;}
-	int* p1{m1}, * p2{m2};
+	int m1[N], m2[M];
+	for (unsigned i = 0; i < N; i++) m1[i] = rand() % 101; 
+	for (unsigned i = 0; i < M; i ++) m2[i] = rand() % 11;
+
+	int* p1{ m1 }, * p2{ m2 };
 	show(p1, N);
-	sorted(p1);
-	show(p1, N); cout<<endl;
-	show(p2, N);
-	sorted(p2);
-	show(p2, N); cout<<endl;
-	int rmas[2*N];
-	concat(rmas, p1, p2, 2*N);
-	show(rmas, 2*N);
+	sorted(p1, N);
+	show(p1, N); cout << endl;
+	show(p2, M);
+	sorted(p2, M);
+	show(p2, M); cout << endl;
+	short r;
+	int* rmas = concat(p1, N, p2, M, r);
+	show(rmas, r);
 }
 
-void show(int* ptr, int k){
-	for (unsigned i = 0; i < k; i++){
+void show(int* ptr, short k){
+	for (unsigned i = 0; i < k; i++) {
 		printf("%2i |", ptr[i]);
 	}
-	cout<<endl;
+	cout << endl;
 }
 
-void sorted(int* mas){
-	unsigned i,j,k;
-	for(i=0; i<N; i++){
-		for(j=N-1; j>i; j--){
-			if(mas[j]<mas[j-1]){
-				k = mas[j-1];
-//				cout<<"j = "<<j<<endl;
-//				printf("After: k = %i, mas[j-1] = %i, mas[j] = %i\n", k, mas[j-1], mas[j]);
-				mas[j-1] = mas[j];
+void sorted(int* mas, short x) {
+	unsigned i, j, k;
+	for (i = 0; i < x; i++) {
+		for (j = x - 1; j > i; j--) {
+			if (mas[j] < mas[j - 1]) {
+				k = mas[j - 1];
+				mas[j - 1] = mas[j];
 				mas[j] = k;
-//				printf("Before: k = %i, mas[j-1] = %i, mas[j] = %i\n", k, mas[j-1], mas[j]);
 			}
 		}
 	}
-//	cout<<"After sorted: ";
-//	for(i = 0; i < N; i++){
-//		cout<<mas[i]<<" ";
-//	}
 }
 
-void concat(int rmas[], int* mas1, int* mas2, int k){
-	for(unsigned i = 0; i<k; i++){
-		if (i<k/2){
-			rmas[i] = mas1[i];
-		}
-		else{
-			rmas[i] = mas2[i%(k/2)];
-		}
+int* concat(int* mas1, short a, int* mas2, short b, short& r) {
+	r = a + b;
+	int* rmas = new int[r];
+	for (short x = 0; x < a; x++) {
+		rmas[x] = mas1[x];
 	}
+	for (short x = 0; x < b; x++) {
+		rmas[x + a] = mas2[x];
+	}
+	return rmas;
 }
